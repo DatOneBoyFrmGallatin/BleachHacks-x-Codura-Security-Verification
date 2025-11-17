5c42a19be2d50624ab94e865a92d78d86f365d33
𝚈𝙶 𝚂𝚖𝚘𝚔𝚊😮‍💨 <b4staysnapping@gmail.com>
2025-11-17 09:22:00 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-
 module.exports = {
   guildId: process.env.GUILD_ID,
+  clientId: process.env.CLIENT_ID,
   verifyChannelId: process.env.VERIFY_CHANNEL_ID,
   memberRoleId: process.env.MEMBER_ROLE_ID,
-  logsChannelId: process.env.LOGS_CHANNEL_ID,
+  logsChannelId: process.env.LOG_CHANNEL_ID,
   giveawayChannelId: process.env.GIVEAWAY_CHANNEL_ID,
   adminRoleId: process.env.ADMIN_ROLE_ID,
   modRoleId: process.env.MOD_ROLE_ID || process.env.ADMIN_ROLE_ID
