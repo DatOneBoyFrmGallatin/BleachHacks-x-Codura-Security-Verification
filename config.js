cb9446c6b3d5cb0db33d99c4c091d8a029d87dec
𝚈𝙶 𝚂𝚖𝚘𝚔𝚊😮‍💨 <b4staysnapping@gmail.com>
2025-11-17 09:31:43 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,5 @@
 module.exports = {
-token: process.env TOKEN,
-  clientId:'YOUR_APPLICATION_ID'
-    guildId: 'YOUR_SERVER_ID'
-  verifyChannelId: process.env.VERIFY_CHANNEL_ID,
-  memberRoleId: process.env.MEMBER_ROLE_ID,
-  logsChannelId: process.env.LOG_CHANNEL_ID,
-  giveawayChannelId: process.env.GIVEAWAY_CHANNEL_ID,
-  adminRoleId: process.env.ADMIN_ROLE_ID,
-  modRoleId: process.env.MOD_ROLE_ID || process.env.ADMIN_ROLE_ID
+    token: process.env.TOKEN,     // This pulls your token from Render
+    clientId: "YOUR_APPLICATION_ID",
+    guildId: "YOUR_SERVER_ID",
 };
