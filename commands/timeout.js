83918c975d00a8d76990b66507a68a7fde60f76a
𝚈𝙶 𝚂𝚖𝚘𝚔𝚊😮‍💨 <b4staysnapping@gmail.com>
2025-11-17 09:35:27 -0800

    Update timeout.js

--- commands/timeout.js
+++ commands/timeout.js
@@ -1,1 +1,39 @@
-module.exports={name:'timeout',execute(){}};
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('timeout')
+        .setDescription('Timeout a user for a specified duration')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('User to timeout')
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('duration')
+                .setDescription('Duration in minutes')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('reason')
+                .setDescription('Reason for timeout')
+                .setRequired(false))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+
+    async execute(interaction) {
+        const user = interaction.options.getUser('user');
+        const duration = interaction.options.getInteger('duration');
+        const reason = interaction.options.getString('reason') || 'No reason provided';
+
+        const member = await interaction.guild.members.fetch(user.id).catch(() => null);
+        if (!member) {
+            return interaction.reply({ content: '❌ Could not find that user.', ephemeral: true });
+        }
+
+        const ms = duration * 60 * 1000;
+
+        await member.timeout(ms, reason).catch(() => {
+            return interaction.reply({ content: '❌ Failed to timeout user.', ephemeral: true });
+        });
+
+        interaction.reply(`⏳ **${user.tag}** has been timed out for **${duration} minutes**.\nReason: *${reason}*`);
+    }
+};
