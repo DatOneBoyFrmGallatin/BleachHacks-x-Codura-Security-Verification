08bd6d31c22c2cb16d5c3b735df19631ddd23dcd
𝚈𝙶 𝚂𝚖𝚘𝚔𝚊😮‍💨 <b4staysnapping@gmail.com>
2025-11-17 09:33:32 -0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,12 @@
-module.exports={name:'ping',execute(){}};
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Shows the bot latency.'),
+
+    async execute(interaction) {
+        const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+        interaction.editReply(`🏓 Pong! Latency: **${sent.createdTimestamp - interaction.createdTimestamp}ms**`);
+    }
+};
