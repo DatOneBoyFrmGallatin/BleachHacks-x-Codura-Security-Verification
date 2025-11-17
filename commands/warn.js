7f4e79a5f998257a3a5f592f54e53adeefe4c431
𝚈𝙶 𝚂𝚖𝚘𝚔𝚊😮‍💨 <b4staysnapping@gmail.com>
2025-11-17 09:36:55 -0800

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,1 +1,23 @@
-module.exports={name:'warn',execute(){}};
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('warn')
+        .setDescription('Warn a user in the server')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('User to warn')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('reason')
+                .setDescription('Reason for the warning')
+                .setRequired(false))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+
+    async execute(interaction) {
+        const user = interaction.options.getUser('user');
+        const reason = interaction.options.getString('reason') || 'No reason provided';
+
+        interaction.reply(`⚠️ **${user.tag}** has been warned.\n📄 Reason: *${reason}*`);
+    }
+};
