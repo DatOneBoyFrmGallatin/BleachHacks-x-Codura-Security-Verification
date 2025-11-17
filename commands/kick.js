45737841a7e4d980889c9bd6ead639a67fd44ee7
𝚈𝙶 𝚂𝚖𝚘𝚔𝚊😮‍💨 <b4staysnapping@gmail.com>
2025-11-17 09:34:55 -0800

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,1 +1,32 @@
-module.exports={name:'kick',execute(){}};
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('kick')
+        .setDescription('Kick a user from the server')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('User to kick')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('reason')
+                .setDescription('Reason for kick')
+                .setRequired(false))
+        .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+
+    async execute(interaction) {
+        const user = interaction.options.getUser('user');
+        const reason = interaction.options.getString('reason') || 'No reason provided';
+
+        const member = await interaction.guild.members.fetch(user.id).catch(() => null);
+        if (!member) {
+            return interaction.reply({ content: '❌ I cannot find that user in the server.', ephemeral: true });
+        }
+
+        await member.kick(reason).catch(err => {
+            return interaction.reply({ content: '❌ Failed to kick user.', ephemeral: true });
+        });
+
+        interaction.reply(`⚠️ **${user.tag}** has been kicked.\n📄 Reason: *${reason}*`);
+    }
+};
