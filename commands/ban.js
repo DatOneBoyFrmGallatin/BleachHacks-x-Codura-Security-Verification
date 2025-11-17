74db1c0301a0c1dac634f20767298ed3db8f4ea0
𝚈𝙶 𝚂𝚖𝚘𝚔𝚊😮‍💨 <b4staysnapping@gmail.com>
2025-11-17 08:44:41 -0800

    Add files via upload

---
+++ commands/ban.js
@@ -0,0 +1,5 @@
+// Ban command
+module.exports = {
+ name: 'ban',
+ execute(){}
+};
--- commands/verify.js
+++ commands/verify.js
@@ -1,16 +1,1 @@
-
-const { SlashCommandBuilder, AttachmentBuilder, PermissionFlagsBits } = require('discord.js');
-const config = require('../config');
-
-module.exports = {
-  data: new SlashCommandBuilder().setName('verify').setDescription('Verify yourself'),
-  async execute(interaction) {
-    if (interaction.channelId !== config.verifyChannelId)
-      return interaction.reply({content:'Use this in the verify channel.', ephemeral:true});
-
-    const banner = new AttachmentBuilder('./assets/verify-banner.jpeg');
-    await interaction.reply({files:[banner], content:'You are now verified!'});
-    const role = interaction.guild.roles.cache.get(config.memberRoleId);
-    await interaction.member.roles.add(role);
-  }
-};
+module.exports={name:'verify',execute(){}};
