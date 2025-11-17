22e9ea80718666b76d5c6c23927814c87042ae16
𝚈𝙶 𝚂𝚖𝚘𝚔𝚊😮‍💨 <b4staysnapping@gmail.com>
2025-11-17 09:37:33 -0800

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,1 +1,32 @@
-module.exports={name:'purge',execute(){}};
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('purge')
+        .setDescription('Delete a number of recent messages')
+        .addIntegerOption(option =>
+            option.setName('amount')
+                .setDescription('How many messages to delete (1–100)')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+
+    async execute(interaction) {
+        const amount = interaction.options.getInteger('amount');
+
+        if (amount < 1 || amount > 100) {
+            return interaction.reply({
+                content: '❌ Amount must be between **1** and **100**.',
+                ephemeral: true
+            });
+        }
+
+        await interaction.channel.bulkDelete(amount, true).catch(() => {
+            return interaction.reply({
+                content: '❌ Could not delete messages in this channel.',
+                ephemeral: true
+            });
+        });
+
+        interaction.reply(`🧹 Deleted **${amount}** messages.`);
+    }
+};
