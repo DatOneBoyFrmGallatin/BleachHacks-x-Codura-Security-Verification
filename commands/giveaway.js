0b97ca627ed1bfad8711ee51022a211feb17a7e4
𝚈𝙶 𝚂𝚖𝚘𝚔𝚊😮‍💨 <b4staysnapping@gmail.com>
2025-11-17 09:38:01 -0800

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -1,4 +1,23 @@
-// Giveaway command
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
 module.exports = {
- name: 'giveaway', execute(){}
+    data: new SlashCommandBuilder()
+        .setName('giveaway')
+        .setDescription('Start a simple giveaway')
+        .addStringOption(option =>
+            option.setName('prize')
+                .setDescription('Prize for the giveaway')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageEvents),
+
+    async execute(interaction) {
+        const prize = interaction.options.getString('prize');
+
+        const message = await interaction.reply({
+            content: `🎉 **GIVEAWAY STARTED!** 🎉\n\nPrize: **${prize}**\nReact with 🎉 to enter!`,
+            fetchReply: true
+        });
+
+        await message.react('🎉');
+    }
 };
