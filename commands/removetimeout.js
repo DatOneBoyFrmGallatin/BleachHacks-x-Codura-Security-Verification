c5b01e252a6508189740527933df4a1b28848aa8
𝚈𝙶 𝚂𝚖𝚘𝚔𝚊😮‍💨 <b4staysnapping@gmail.com>
2025-11-17 09:36:07 -0800

    Update removetimeout.js

--- commands/removetimeout.js
+++ commands/removetimeout.js
@@ -1,1 +1,27 @@
-module.exports={name:'removetimeout',execute(){}};
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('removetimeout')
+        .setDescription('Remove a timeout from a user')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('User to remove timeout from')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+
+    async execute(interaction) {
+        const user = interaction.options.getUser('user');
+
+        const member = await interaction.guild.members.fetch(user.id).catch(() => null);
+        if (!member) {
+            return interaction.reply({ content: '❌ Could not find that user.', ephemeral: true });
+        }
+
+        await member.timeout(null).catch(() => {
+            return interaction.reply({ content: '❌ Failed to remove timeout.', ephemeral: true });
+        });
+
+        interaction.reply(`🔓 Timeout removed from **${user.tag}**.`);
+    }
+};
